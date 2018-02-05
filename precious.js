63bb9453463f42014d22ab31a77bf794b7c1409e
Precious <216110097@students.stamford.edu>
2018-02-05 18:10:59 +0700

    assignment 4

---
+++ precious.js
@@ -0,0 +1,18 @@
+var customername = "Peary"
+var price = 10;
+var quantity = 3;
+var totalPrice = price * quanttity;
+document.getElementById("customer-name").innerHTML = "Peary";
+document.getElementById("price").innerHTML = "$"+ totalPrice;
+var products = [
+ "Brownie";
+ "Blackout cake";
+ "Charlotte";
+ "Caterpillar cake";
+ "Depression cake";
+
+];
+var price = [15,20,30,80,100];
+
+var productsText = "";
+var productsText
